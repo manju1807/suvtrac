e6ad28b567d90cf836b2002b35631c0febb7b57f
manju1807 <rmanjunath18@outlook.com>
2023-08-22 21:00:52 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 const App = () => {
-  return <div></div>;
-};
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+  };
+
 
 export default App;
