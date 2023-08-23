fce5c92ea0a6eb054b519adeb617cc8acfbad173
anushree <anushree9019@gmail.com>
2023-08-23 18:34:32 +0530

    first

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,7 +1,36 @@
 const Settings = () => {
   return (
-    <div className="top-[5rem] text-3xl text-red-700">
-      <h1>settings</h1>
+    <div className="bg-gray-100 flex flex-col lg:grid grid-cols-3 max-w-full gap-2 text-white m-2 ">
+      <div className="bg-white flex flex-col ">
+        <div className="font-bold text-xl text-black">System Alarms</div>
+        <div className= "text-black m-3">Low Temperature Alarm</div>
+        <div className= "flex flex-col bg-blue-100 m-3 p-2 rounded-t-md border-b-2 border-black border-opacity-25 ">
+        <div className="text-sm text-gray-700">Low Temperature Threshold</div>
+        <div className="text-black">10</div>
+        </div>
+        <div className= "text-black m-3">Exceeding Daily Consumption Alarm</div>
+        <div className= "flex flex-col bg-blue-100 m-3 p-2 rounded-t-md border-b-2 border-black border-opacity-25 ">
+        <div className="text-sm text-gray-700"> Daily Consumption Threshold</div>
+        <div className="text-black">18</div>
+        </div>
+        <div className= "text-black m-3">Exceeding Weekly Consumption Alarm</div>
+        <div className= "flex flex-col bg-blue-100 m-3 p-2 rounded-t-md border-b-2 border-black border-opacity-25 ">
+        <div className="text-sm text-gray-700"> Weekly Consumption Threshold</div>
+        <div className="text-black ">353</div>
+        </div>
+        </div>
+        <div className="bg-white flex flex-col ">
+        <div className="font-bold text-xl text-black m-2">SMS Notification</div>
+        <div className= "text-gray-500 m-3">Low Temperature Alarm</div>
+        <div className= "text-gray-500 m-3">Exceeding Daily Consumption Alarm</div>
+        <div className= "text-gray-500 m-3">Exceeding Weekly Consumption Alarm</div>
+        </div>
+        <div className="bg-white flex flex-col ">
+        <div className="font-bold text-xl text-black">Email Notifications</div>
+        <div className= "text-gray-500 m-3">Low Temperature Alarm</div>
+        <div className= "text-gray-500 m-3">Exceeding Daily Consumption Alarm</div>
+        <div className= "text-gray-500 m-3">Exceeding Weekly Consumption Alarm</div>
+        </div>
     </div>
   );
 };
