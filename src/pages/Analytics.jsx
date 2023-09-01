eddad4e4da0b0ea1ed5a5e9b265fe40ca79aeaad
anushree <anushree9019@gmail.com>
2023-09-01 14:46:34 +0530

    third

--- src/pages/Analytics.jsx
+++ src/pages/Analytics.jsx
@@ -3,8 +3,17 @@ import { BsList } from 'react-icons/bs';
 
 const Analytics = () => {
   return (
-    <div className="bg-red-400 flex flex-col h-[100%] max-w-[100%] gap-2 md:grid lg:grid lg:grid-cols-3 lg:grid-rows-1 m-2">
-      <div className="bg-sky-300 lg:col-span-2 lg:row-span-1">hello</div>
+    <div className="bg-white flex flex-col h-[100%] max-w-[100%] gap-2 md:grid lg:grid lg:grid-cols-3 lg:grid-rows-1 m-2">
+      <div className="bg-white lg:col-span-2 lg:row-span-1 flex h-28 ">
+    <div className="bg-white flex-grow mr-2 border border-gray-200">
+      <div className="mt-6 font-bold text-2xl text-center">518388.54 m</div>
+      <div className="mt-1 text-sm font-normal text-center">Consumed per day</div>
+    </div>
+    <div className="bg-white flex-grow mr-2 border border-gray-200">
+      <div className="mt-6 font-bold text-2xl text-center">518388.54 m</div>
+      <div className="mt-1 text-sm font-normal text-center">Consumed per day</div>
+    </div>
+  </div>
       <div className="bg-white lg:col-span-1 lg:row-span-1  h-screen border border-gray-200">
       <div className=' flex items-center justify-between font-bold text-xl text-black p-3'>
       Water Meters
